5d8e1dce9e60e4ea1969dc846e36fb4fad49158c
Marat Miftakhov <m.miftahov@gmail.com>
2025-06-30 15:46:23 +0200

    version 0.1.3

--- config/elastic.php
+++ config/elastic.php
@@ -12,13 +12,7 @@ return [
     */
 
     'hosts' => [
-        [
-            'host' => env('ELASTICSEARCH_HOST', 'localhost'),
-            'port' => env('ELASTICSEARCH_PORT', 9200),
-            'scheme' => env('ELASTICSEARCH_SCHEME', 'http'),
-            'user' => env('ELASTICSEARCH_USER', null),
-            'pass' => env('ELASTICSEARCH_PASS', null),
-        ],
+        env('ELASTICSEARCH_HOST_FULL', 'http://localhost:9200'),
     ],
 
     /*
